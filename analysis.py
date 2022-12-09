f8b668c9b1cf7554cb1a1836919f9567ff141484
David Lu <davidboxboro@gmail.com>
2022-12-08 19:10:40 -0500

    faster opt

--- analysis.py
+++ analysis.py
@@ -44,9 +44,13 @@ def mk_df():
     return df
 
 
-def optimize(nut_targs, df, T):
+def optimize(nut_bounds, df, T):
+    # sample df to make it faster
+    df = df.sample(frac=0.2)
+
     # variables
     I = len(df)
+    print(f'{I} dishes to choose from')
     x = cp.Variable((I, T), boolean=True)
     s = cp.Variable((I, T), nonneg=True)
 
@@ -60,11 +64,11 @@ def optimize(nut_targs, df, T):
     constraints.append(s <= 3 * x)  # meals can't be more than 3 servings
     constraints.append(s >= 0.5 * x)  # meals can't be less than 0.5 servings
     constraints.append(cp.sum(x, axis=0) == 3)  # 3 meals per day
-    constraints.append(cp.sum(x, axis=1) <= max(1, T // 7))  # no meals per day
+    constraints.append(cp.sum(x, axis=1) <= max(1, T * (2 / 7)))  # don't repeat meals too often
     # macros lower and upper limits
     for t in range(T):
-        for nut in nut_targs:
-            lo, hi = nut_targs[nut]
+        for nut in nut_bounds:
+            lo, hi = nut_bounds[nut]
             lo_constraint = cp.sum(cp.multiply(s[:, t], df[nut])) >= lo
             hi_constraint = cp.sum(cp.multiply(s[:, t], df[nut])) <= hi
             constraints += [lo_constraint, hi_constraint]
@@ -72,7 +76,6 @@ def optimize(nut_targs, df, T):
     # solve
     prob = cp.Problem(obj, constraints)
     cost_opt = prob.solve(solver=cp.GUROBI, verbose=1)
-    x_opt = x.value
     s_opt = s.value
 
     # stats
@@ -92,18 +95,23 @@ def optimize(nut_targs, df, T):
 if __name__ == '__main__':
     # read data into DF
     df = mk_df()
-    N = len(df)
-    print(f'{N} dishes to choose from')
 
     nut_targs = {
-        'calories': [2450, 2550],
-        'carbohydrates': [309, 329],
-        'protein': [103, 123],
-        'fat': [82, 102],
-        'fiber': [25, 30],
-        'sodium': [0, 1500],
+        'calories': 2500,
+        'carbohydrates': 319,
+        'protein': 113,
+        'fat': 92,
+        'fiber': 27.5,
     }
+    nut_bounds = {
+        'sodium': [0, 2500],
+    }
+    for nut, targ in nut_targs.items():
+        lb = 0.9 * targ
+        ub = 1.1 * targ
+        nut_bounds[nut] = [lb, ub]
+    print(nut_bounds)
 
-    D = 2
-    df_opt, cost_opt = optimize(nut_targs, df, D)
+    D = 7
+    df_opt, cost_opt = optimize(nut_bounds, df, D)
     df_opt.to_csv('opt.csv')
